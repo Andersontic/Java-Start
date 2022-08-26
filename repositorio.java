c6955439840be01fb44cb10e03c7918dcdb71079
Fr34k303 <111389064+Fr34k303@users.noreply.github.com>
2022-08-26 13:12:51 -0500

    Update repositorio.java

--- repositorio.java
+++ repositorio.java
@@ -1,7 +1,6 @@
 public class repositorio{
   public static void main(String args[]){
-    
-   System.out.println("Prueba Oscar Moreno Github Spring1 Java-Start";
    System.out.println("Bienvenidos al repositorio Java-Start"); 
+   System.out.println("Prueba Oscar Moreno Github Spring1 Java-Start";
  }
 }
