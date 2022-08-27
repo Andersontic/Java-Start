516e1776641303087a30362f8bc309d4986615e2
TorLaT <latoro@mail.uniatlantico.edu.co>
2022-08-26 20:25:41 -0500

    Estoy probando Intellij Idea

--- repositorio.java
+++ repositorio.java
@@ -1,6 +1,6 @@
 public class repositorio{
   public static void main(String args[]){
-
+//Estoy probando github con intellij idea
    System.out.println("Bienvenidos al repositorio Java-Start");
    System.out.println("Actualizando la Rama de Luis Toro";
   }
