7e696b014f459a923ebfa493e9b9983d7d2ff95f
TorLaT <latoro@mail.uniatlantico.edu.co>
2022-08-26 22:39:57 -0500

    subiendo archivos e spring boot

---
+++ src/main/java/com/ciclo3Udea/JavaStart/JavaStartApplication.java
@@ -0,0 +1,13 @@
+package com.ciclo3Udea.JavaStart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaStartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ciclo3Udea/JavaStart/JavaStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciclo3Udea.JavaStart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
