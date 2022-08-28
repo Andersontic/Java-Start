389bcc58e8ed9ed1138e751835921cd4bca2d601
Harol Sanchez <harolsanchez64@gmail.com>
2022-08-28 18:33:01 -0500

    Creacion Clase Empleado (sujeta a cambios)

--- repositorio.java
+++ repositorio.java
@@ -4,3 +4,65 @@ public class repositorio{
    System.out.println("Bienvenidos al repositorio Java-Start"); 
  }
 }
+
+
+public class Empleado{
+
+    private int id;
+    private String nombre;
+    private String correo;
+    private Empresa empresa;
+    private String rol;
+
+    public Empleado() {
+    }
+
+    public Empleado(String nombre, String correo, Empresa empresa, String rol) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.empresa = empresa;
+        this.rol = rol;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public Empresa getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(Empresa empresa) {
+        this.empresa = empresa;
+    }
+
+
+}
