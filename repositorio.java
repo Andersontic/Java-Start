475edf79640cd4fa72611518c0ea69a563258272
Fr34k303 <oskarmr.aprendizaje@gmail.com>
2022-08-30 19:27:45 -0500

    Nombre Correo Administrador operativo,

--- repositorio.java
+++ repositorio.java
@@ -4,3 +4,37 @@ public class repositorio{
    System.out.println("Prueba Oscar Moreno Github Spring1 Java-Start";
  }
 }
+enum TipoEmpresa_Enum{
+    ADMINISTRADOR, OPERATIVO
+}
+class Empleado{
+    private String Nombre;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String correo) {
+        Correo = correo;
+    }
+
+    public TipoEmpresa_Enum getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoEmpresa_Enum tipo) {
+        this.tipo = tipo;
+    }
+
+    private String Correo;
+    private TipoEmpresa_Enum tipo;
+}
+
